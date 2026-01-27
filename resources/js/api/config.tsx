92423c05a2e39de5e7bb037b8896e7e1d5a9c931
Алексей Михайлов <okayfantasy@gmail.com>
2026-01-27 14:38:09 +0300

    правки конфига

--- resources/js/api/config.tsx
+++ resources/js/api/config.tsx
@@ -1,17 +1,7 @@
 export const getApiBaseUrl = (): string => {
-    if (import.meta.env.APP_URL) {
-        return import.meta.env.APP_URL;
+    if (import.meta.env.VITE_API_BASE_URL) {
+        return import.meta.env.VITE_API_BASE_URL;
     }
 
-    // Автоопределение по NODE_ENV
-    switch (import.meta.env.APP_ENV) {
-        case "dev":
-            return "http://localhost:8000/-/api";
-        case "staging":
-            return "https://staging.yourdomain.com/-/api";
-        case "production":
-            return "https://yourdomain.com/-/api";
-        default:
-            return "/-/api";
-    }
+    return "/-/api";
 };
